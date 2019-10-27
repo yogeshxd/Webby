eb991e464a7824eb3d9956f2694087253a92ec5a
Yogesh Gupta <45172882+Yogesh512@users.noreply.github.com>
2019-10-27 13:55:45 +0530

    Add files via upload

---
+++ Webby.py
@@ -0,0 +1,81 @@
+import os
+import time
+try:
+    import requests
+    import urllib.request
+    from bs4 import BeautifulSoup
+    print('All Libraries Installed.\nStarting Scraper')
+except:
+    print('All Libraries not installed')
+    a = input('Please Install All libraries And Come Back')
+    exit(0)
+
+def scraper(url,t):
+    try:
+        os.remove('RAW_1.txt')
+        os.remove('RAW_2.txt')
+        print('Cleared Chached Data')
+    except:
+        print('Cleared Chached Data')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    file = open('RAW_1.txt','wb')
+    file.write(str(soup.findAll()).encode("utf-8"))
+    file.close()
+    print('Fetched')
+    time.sleep(t)
+    file2 = open('RAW_2.txt','wb')
+    file2.write(str(soup.findAll()).encode("utf-8"))
+    file2.close()
+    print('Started Comparing')
+    f1 = open('RAW_1.txt', 'rb')
+    a = str(f1)
+    c = a.split()
+    f1.close()
+    f2 = open('RAW_2.txt','rb')
+    b = str(f2)
+    d = b.split()
+    f2.close()
+    with open("RAW_1.txt",'rb') as f:
+      lines = f.read()
+    with open("RAW_2.txt",'rb') as f:
+      lines2 = f.read()
+    l1 = []
+    l2 = []
+    for a in lines:
+        c = str(a)
+        words = c.strip().split()
+        l1.append(words)
+    for b in lines2:
+        d = str(b)
+        w = d.strip().split()
+        l2.append(w)
+    print('Compared')
+    if l1!=l2:
+        return yes
+    os.remove('RAW_1.txt')
+    os.remove('RAW_2.txt')
+    time.sleep(1)
+
+#main_block
+url = input('Enter The URL To Scrape\n>>')
+ask  = input('Do you Want To Run The Script Infinetly(y/n)\n>>')
+content = 'A Change Was Detected On Given URL'
+
+if ask == 'y' or ask == 'Y' or ask == 'Yes':
+    t = int(input('Enter Time Interval B/W 2 Scraps(in sec):\n>>'))
+    while True:
+        if scraper(url,t)=='Yes':
+            print(content)
+        else:
+            print('No Changes Detected')
+        
+else:
+    print("Default Time Interval Is 10 Sec")
+    if scraper(url,10)=='Yes':
+            print(content)
+            a = input('Hit Enter To Exit')
+    else:
+        print('No Changes Detected')
+        a = input('Hit Enter To Exit')
+    
